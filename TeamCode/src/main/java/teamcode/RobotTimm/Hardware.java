8651d98f4da11e0d06ba0f78e2d4684f10c206d4
mihneabondor <mihnea.bondor@icloud.com>
2023-11-26 11:05:39 +0200

    Nostalgie de driver si coach @vladd23

--- TeamCode/src/main/java/teamcode/RobotTimm/Hardware.java
+++ TeamCode/src/main/java/teamcode/RobotTimm/Hardware.java
@@ -2,6 +2,7 @@ package teamcode.RobotTimm;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,10 +13,6 @@ public class Hardware {
     public DcMotorEx motordreaptafata;
     public DcMotorEx motorstangaspate;
     public DcMotorEx motorstangafata;
-    public DcMotorEx motorglisiera1;
-    public DcMotorEx motorglisiera2;
-    public Servo servoincheietura;
-    public Servo servogripper;
 
     public Hardware(HardwareMap hardwareMap) {
         motordreaptafata = hardwareMap.get(DcMotorEx.class, "dreapta fata");
@@ -23,22 +20,13 @@ public class Hardware {
         motorstangafata = hardwareMap.get(DcMotorEx.class, "stanga fata");
         motorstangaspate = hardwareMap.get(DcMotorEx.class, "stanga spate");
 
-        motorglisiera1 = hardwareMap.get(DcMotorEx.class, "glisiera stanga");
-        motorglisiera2 = hardwareMap.get(DcMotorEx.class, "glisiera dreapta");
-
-        servogripper = hardwareMap.get(Servo.class, "servo gripper");
-        servoincheietura = hardwareMap.get(Servo.class, "servo incheietura");
-
-        servoincheietura.setPosition(0.5);
-        servogripper.setPosition(0.5);
-
         motordreaptafata.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorstangafata.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motordreaptaspate.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorstangaspate.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        motorglisiera1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motorglisiera2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorstangaspate.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorstangafata.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
